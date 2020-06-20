ec6bab0c36e4acb49263f42d1b5c55c186e79018
2020-DEV-075 <2020-DEV-075>
2020-06-20 23:29:10 +0200

    added tileButton action and outlets for rowStackViews

--- TicTacToe/Game/GameViewController.swift
+++ TicTacToe/Game/GameViewController.swift
@@ -18,16 +18,28 @@ final class GameViewController: UIViewController {
         fatalError("init(coder:) has not been implemented")
     }
     
+    //MARK:- Outlets
+    
     @IBOutlet weak var tilesView: UIView!
+    
     @IBOutlet weak var tilesHolderStackView: UIStackView!
+    @IBOutlet weak var rowOneStackView: UIStackView!
+    @IBOutlet weak var rowTwoStackView: UIStackView!
+    @IBOutlet weak var rowThreeStackView: UIStackView!
+    
     @IBOutlet var tileButtons: [UIButton]!
+    
     @IBOutlet weak var infoLabel: UILabel!
+    
+    //MARK:- Life Cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         setupUI()
     }
+    
+    //MARK:- Methods
     
     private func setupUI() {
         self.navigationItem.title = "TicTacToe"
@@ -38,8 +50,14 @@ final class GameViewController: UIViewController {
         tilesHolderStackView.addBackground(color: .black)
     }
     
+    //MARK:- Actions
+    
     @objc func replayButtonTapped() {
         
     }
+    
+    @IBAction func tileAction(_ sender: UIButton) {
+    }
+    
 
 }
