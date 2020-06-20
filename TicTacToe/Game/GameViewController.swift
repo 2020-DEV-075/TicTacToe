eccee030673b1e34e656770db984b9eca7c5d5d9
2020-DEV-075 <2020-DEV-075>
2020-06-20 23:12:42 +0200

    setup tiles UI

--- TicTacToe/Game/GameViewController.swift
+++ TicTacToe/Game/GameViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class GameViewController: UIViewController {
+final class GameViewController: UIViewController {
     
     init() {
         super.init(nibName: nil, bundle: nil)
@@ -17,22 +17,14 @@ class GameViewController: UIViewController {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+    @IBOutlet weak var tilesView: UIView!
+    @IBOutlet weak var tilesHolderStackView: UIStackView!
+    @IBOutlet var tileButtons: [UIButton]!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
 }
