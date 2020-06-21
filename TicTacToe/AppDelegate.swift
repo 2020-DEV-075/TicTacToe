3413bdbaecaa6276708c00e9dbec3ef971706bda
2020-DEV-075 <67195267+2020-DEV-075@users.noreply.github.com>
2020-06-21 17:05:45 +0200

    Revert "Feat/plug logic ui"

--- TicTacToe/AppDelegate.swift
+++ TicTacToe/AppDelegate.swift
@@ -16,7 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
-        let gameViewController = GameViewController(game: Game())
+        let gameViewController = GameViewController()
         navigationController = UINavigationController(rootViewController: gameViewController)
         
         window = UIWindow(frame: UIScreen.main.bounds)
